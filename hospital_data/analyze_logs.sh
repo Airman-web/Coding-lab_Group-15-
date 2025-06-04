982c9f90eb5f28749fa1bcfa065c7d21861f8acb
Airman-web <e.atigbi@alustudent.com>
2025-06-04 02:51:51 -0700

    deleted archived_logs

--- hospital_data/analyze_logs.sh
+++ hospital_data/analyze_logs.sh
@@ -1,2 +1,10 @@
 #!/bin/bash
+echo "Select log file to analyze:"
+echo "1) Heart Rate (heart_rate.log)"
+echo "2) Temperature (temperature.log)"
+echo "3) Water Usage (water_usage.log)"
+read -p  "Enter choice (1-3): " choice 
+case $choice in 
+	file="hospital_data/active_logs/heart_rate.log"; ;
+
 
