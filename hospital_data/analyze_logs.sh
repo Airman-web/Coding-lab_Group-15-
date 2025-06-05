726e5b9a82b21e53569cb2122519a73737c5b61d
lisaineza <l.ineza@alustudent.com>
2025-06-05 12:25:45 +0200

    added user prompt for analyzing and created report directory

--- hospital_data/analyze_logs.sh
+++ hospital_data/analyze_logs.sh
@@ -1,9 +1,16 @@
 #!/bin/bash
-echo "Select log file to analyze:"
-echo "1) Heart Rate (heart_rate.log)"
-echo "2) Temperature (temperature.log)"
-echo "3) Water Usage (water_usage.log)"
-read -p  "Enter choice (1-3): " choice 
-case $choice in 
-	file="hospital_data/active_logs/heart_rate.log"; ;
-	file="hospital_data/active_logs/temperature
+
+echo 'Select log file to analyze:'
+echo '1) Heart Rate (heart_rate.log)'
+echo '2) Temperature (temperature.log)'
+echo '3) Water Usage (water_usage.log)'
+
+read -p 'Enter choice (1-3): ' user_choice
+
+# Create reports directory if it doesn't exist for all reports
+reports_dir='reports'
+if [ ! -d "$reports_dir" ]; then
+    mkdir "$reports_dir"
+fi
+
+
