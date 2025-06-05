4d2d3a14f842987985da23ad29e0485343cd3dfd
lisaineza <l.ineza@alustudent.com>
2025-06-05 12:26:26 +0200

    created case statements for log file and types based on user choice

--- hospital_data/analyze_logs.sh
+++ hospital_data/analyze_logs.sh
@@ -13,4 +13,23 @@ if [ ! -d "$reports_dir" ]; then
     mkdir "$reports_dir"
 fi
 
+# log files based on user choice
+case $user_choice in
+    1)
+        log_file='active_logs/heart_rate_log.log'
+        log_type='Heart Rate'
+        ;;
+    2)
+        log_file='active_logs/temperature_log.log'
+        log_type='Temperature'
+        ;;
+    3)
+        log_file='active_logs/water_usage_log.log'
+        log_type='Water Usage'
+        ;;
+    *)
+        echo -e "\nInvalid choice. Please enter 1, 2, or 3."
+        exit 1
+        ;;
 
+esac
